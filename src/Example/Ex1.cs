5b33b658d1666ccbbae66b5af0dc6ac30a89c3e3
Jens Tirsvad Nielsen <jenstirsvad@gmail.com>
2025-03-28 11:24:10 +0800

    Add examples Fix left side frame extra space

---
+++ src/Example/Ex1.cs
@@ -0,0 +1,36 @@
+﻿using TirsvadCLI;
+
+namespace Example;
+
+/// <summary>
+/// Example of how to use the Frame class.
+/// </summary>
+internal class Ex1
+{
+    static void Main(string[] args)
+    {
+        // Create a new frame with a window size of 20x20 and a frame size of 10x10
+        Frame frame = new Frame(windowWidth: 20, windowHeight: 20, frameWidth: 10, frameHeight: 10);
+        frame.SetColorBg(ConsoleColor.DarkBlue); // Set background color of the frame
+        frame.Render(true);
+        Console.WriteLine("\nPress any key to see next example.");
+        Console.ReadKey();
+
+        // Clear the console and create a new frame with a window size of 20x20 and a frame size of 16x10
+        Console.Clear();
+        frame = new Frame(windowWidth: 20, windowHeight: 20, frameWidth: 16, frameHeight: 10);
+        frame.SetFrameText("Hello, World!"); // Set text inside the frame
+        frame.SetColorBg(ConsoleColor.White); // Set background color of the frame
+        frame.SetColorFg(ConsoleColor.Blue); // Set foreground color of the frame
+        frame.Render(true);
+        Console.WriteLine("\nPress any key to see next example.");
+        Console.ReadKey();
+
+        // Clear the console and create a new frame with a window size of 20x20 and a frame size of 10x10
+        Console.Clear();
+        frame = new Frame(windowWidth: 20, windowHeight: 20, frameWidth: 10, frameHeight: 10, leftTop: '*', rightTop: '*', leftBottom: '*', rightBottom: '*', horizontal: '*', vertical: '*');
+        frame.Render(true);
+        Console.WriteLine("\nPress any key to exit.");
+        Console.ReadKey();
+    }
+}
--- src/Frame/Frame/Frame.cs
+++ src/Frame/Frame/Frame.cs
@@ -1,18 +1,19 @@
 ﻿namespace TirsvadCLI;
+
 public class Frame
 {
     private int _windowWidth; // Width of the frame
     private int _windowHeight; // Height of the frame
-    private int _width; // Width of the frame. Can be different from windowWidth when using center frame
-    private int _height; // Height of the frame. Can be different from windowHeight when using center frame
+    private int _frameWidth; // Width of the frame. Can be different from windowWidth when using center frame
+    private int _frameHeight; // Height of the frame. Can be different from windowHeight when using center frame
     private int _startX = 0; // Start position of the frame on the x-axis
     private int _startY = 0; // Start position of the frame on the y-axis
-    private char _leftTop = '╔'; // Character for the top-left corner
-    private char _rightTop = '╗'; // Character for the top-right corner
-    private char _leftBottom = '╚'; // Character for the bottom-left corner
-    private char _rightBottom = '╝'; // Character for the bottom-right corner
-    private char _horizontal = '═'; // Character for the horizontal borders
-    private char _vertical = '║'; // Character for the vertical borders
+    private char _leftTop; // Character for the top-left corner
+    private char _rightTop; // Character for the top-right corner
+    private char _leftBottom; // Character for the bottom-left corner
+    private char _rightBottom; // Character for the bottom-right corner
+    private char _horizontal; // Character for the horizontal borders
+    private char _vertical; // Character for the vertical borders
 
     private List<string>? _frameText; // Title of the frame
 
@@ -20,39 +21,37 @@ public class Frame
     private ConsoleColor FrameColorFg = ConsoleColor.White; // Foreground color of the frame
 
     /// <summary>
-    /// Initializes a new instance of the <see cref="Frame"/> class with specified width and height.
+    /// Full screen frame with default border characters.
+    /// Default the frame will be same size as window if not specified.
     /// </summary>
-    /// <param name="width">The width of the frame.</param>
-    /// <param name="height">The height of the frame.</param>
-    public Frame(int width, int height)
+    /// <param name="windowWidth">The windowWidth of the frame.</param>
+    /// <param name="windowHeight">The windowHeight of the frame.</param>
+    /// <param name="frameWidth">Optional: The width of the frame.</param>
+    /// <param name="frameHeight">Optional: The height of the frame.</param>
+    /// <param name="leftTop">Optional: The character for the top-left corner.</param>
+    /// <param name="rightTop">Optional: The character for the top-right corner.</param>
+    /// <param name="leftBottom">Optional: The character for the bottom-left corner.</param>
+    /// <param name="rightBottom">Optional: The character for the bottom-right corner.</param>
+    /// <param name="horizontal">Optional: The character for the horizontal borders.</param>
+    /// <param name="vertical">Optional: The character for the vertical borders.</param>
+    public Frame(int windowWidth, int windowHeight, int? frameWidth = null, int? frameHeight = null, char leftTop = '╔', char rightTop = '╗', char leftBottom = '╚', char rightBottom = '╝', char horizontal = '═', char vertical = '║')
     {
-        _width = _windowWidth = width;
-        _height = _windowHeight = height;
-    }
-
-    /// <summary>
-    /// Initializes a new instance of the <see cref="Frame"/> class with specified dimensions and border characters.
-    /// </summary>
-    /// <param name="width">The width of the frame.</param>
-    /// <param name="height">The height of the frame.</param>
-    /// <param name="leftTop">The character for the top-left corner.</param>
-    /// <param name="rightTop">The character for the top-right corner.</param>
-    /// <param name="leftBottom">The character for the bottom-left corner.</param>
-    /// <param name="rightBottom">The character for the bottom-right corner.</param>
-    /// <param name="horizontal">The character for the horizontal borders.</param>
-    /// <param name="vertical">The character for the vertical borders.</param>
-    public Frame(int width, int height, char leftTop, char rightTop, char leftBottom, char rightBottom, char horizontal, char vertical)
-    {
-        _windowWidth = width;
-        _windowHeight = height;
-        _width = width;
-        _height = height;
+        _windowWidth = windowWidth;
+        _windowHeight = windowHeight;
         _leftTop = leftTop;
         _rightTop = rightTop;
         _leftBottom = leftBottom;
         _rightBottom = rightBottom;
         _horizontal = horizontal;
         _vertical = vertical;
+        if (frameWidth != null)
+            _frameWidth = frameWidth.Value;
+        else
+            _frameWidth = windowWidth;
+        if (frameHeight != null)
+            _frameHeight = frameHeight.Value;
+        else
+            _frameHeight = windowHeight;
     }
 
     /// <summary>
@@ -76,7 +75,7 @@ public class Frame
     public void SetFrameText(string title)
     {
         _frameText = new List<string>();
-        _height++;
+        _frameHeight++;
         _frameText.Add(title);
     }
 
@@ -85,7 +84,7 @@ public class Frame
         _frameText = new List<string>();
         foreach (string line in title)
         {
-            _height++;
+            _frameHeight++;
             _frameText.Add(line);
         }
     }
@@ -99,8 +98,9 @@ public class Frame
         string text = "";
         if (center)
         {
-            // Do something special for box in the center of the screen
-            throw new NotImplementedException();
+            _startX = (_windowWidth - _frameWidth) / 2;
+            _startY = (_windowHeight - _frameHeight) / 2;
+
         }
 
         Console.BackgroundColor = FrameColorBg;
@@ -109,35 +109,37 @@ public class Frame
         // Draw top border
         Console.SetCursorPosition(_startX, _startY);
         Console.Write(_leftTop);
-        for (int i = 0; i < _width - 1; i++)
+        for (int i = 0; i < _frameWidth - 1; i++)
         {
             Console.Write(_horizontal);
         }
         Console.Write(_rightTop);
 
         // Draw left and right border
-        for (int i = 1; i < _height - 1; i++)
+        for (int i = 1; i < _frameHeight - 1; i++)
         {
-            if (_frameText != null)
-                text = _frameText.ElementAtOrDefault(i - 1) ?? "";
             Console.SetCursorPosition(_startX, _startY + i);
             Console.Write(_vertical);
-            Console.Write(" " + text);
-            // Clear the inside of the box
             Console.BackgroundColor = ConsoleColor.Black;
-            for (int j = 1; j < _width; j++)
+            if (_frameText != null)
+            {
+                text = _frameText.ElementAtOrDefault(i - 1) ?? "";
+                Console.Write(" " + text);
+            }
+            // Clear the inside of the box
+            for (int j = 1; j < _frameWidth; j++)
             {
                 Console.Write(" ");
             }
             Console.BackgroundColor = FrameColorBg;
-            Console.SetCursorPosition(_startX + _width, _startY + i);
+            Console.SetCursorPosition(_startX + _frameWidth, _startY + i);
             Console.Write(_vertical);
         }
 
         // Draw bottom border
-        Console.SetCursorPosition(_startX, _startY + _height - 1);
+        Console.SetCursorPosition(_startX, _startY + _frameHeight - 1);
         Console.Write(_leftBottom);
-        for (int i = 0; i < _width - 1; i++)
+        for (int i = 0; i < _frameWidth - 1; i++)
         {
             Console.Write(_horizontal);
         }
